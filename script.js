9145a8070e6791d0d9ef6dbece4ff09c0c5a1b33
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-07-21 22:04:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 function highlight() {
     //Write your code here
-
-
+	const target=document.getElementsByTagName('strong');
+	console.log(target)
+	
+	// const targetArray = Array.from(target); // Convert HTMLCollection to array
+	for (let i = 0; i < target.length; i++) {
+         target[i].style.color = 'green';
+    }
 }
 
+const para=document.getElementsByTagName("p")[0];
+para.addEventListener("mouseover",highlight);
+console.log(para);
 
 function return_normal() {
     //Write your code here
+	const target=document.getElementsByTagName("strong");
 
-    
+	for (let i = 0; i < target.length; i++) {
+         target[i].style.color = 'black';
+    }
+	
 }
+para.addEventListener("mouseout",return_normal);
